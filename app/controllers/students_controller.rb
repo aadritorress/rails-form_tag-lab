deea79167340ff6ef7cb8408b7eed3e3f57549b8
Adriana <adriana.amaral@msn.com>
2021-03-20 19:19:02 -0500

    commit tree 7eb7533a3123422bc501063944e3157a5fca27fb

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,18 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+
+  def new
+  @student = Student.new
+  end
+
+  # def create
+  #   @student = Student.create(title: params[:student][:first_name], description: params[:student][:last_name])
+  # redirect_to students_path(@student)
+
+  def create
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to student_path(@student)
+  end
+
 end
